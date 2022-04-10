5a764b74c3b055f5e00f27fc079b01d0ec70ceef
Shuning <ya.aif2002@yandex.ru>
2022-04-10 23:29:23 +0300

    Add project files.

---
+++ CMole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YearPrj.Properties;
+using System.Drawing;
+
+namespace YearPrj
+{
+    class CMole:CImageBase
+    {
+        public CMole()
+            : base(Resources.mole)
+        {
+            _moleHotSpot.X = Left + 20;
+            _moleHotSpot.Y = Top - 1;
+            _moleHotSpot.Width = 30;
+            _moleHotSpot.Height = 40;
+        }
+        private Rectangle _moleHotSpot=new Rectangle();
+        public void Update(int X,int Y)
+        {
+            Left=X;
+            Top=Y;
+            _moleHotSpot.X=Left+20;
+            _moleHotSpot.Y=Top-1;
+        }
+        public bool Hit(int X,int Y)
+        {
+            Rectangle c=new Rectangle(X,Y,1,1);
+            if(_moleHotSpot.Contains(c))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
